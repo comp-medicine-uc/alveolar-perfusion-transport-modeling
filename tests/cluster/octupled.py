b2b8b9b22b1f1e5174f89db0eba89a6d0745f24f
Bastián Herrera <biherrera@uc.cl>
2022-07-27 21:07:27 -0400

    Added octuple test
    side = 94

---
+++ tests/cluster/octupled.py
@@ -0,0 +1,39 @@
+'''Main file for alveolar perfusion and gas exchange simulations in TKD
+mesh.
+'''
+
+__author__ = 'biherrera'
+__email__ = 'biherrera@uc.cl'
+
+import sys
+import os
+# The following line adds the directory to the path in order to cross-reference
+# files in the repo
+sys.path.append(os.getcwd()[:-14])
+print("Relative path: ", os.getcwd()[:-14])
+import dolfin
+from src.model import PerfusionGasExchangeModel
+from src.params import params
+
+tol = 1
+side_length = 50
+
+print("Starting...")
+folder = "octupled"
+path = os.path.join("../../raw-and-results-data", folder)
+model = PerfusionGasExchangeModel(folder_path=path, params=params)
+print("Model initialised")
+model.import_mesh(
+    os.path.join("../../raw-and-results-data/random", "octupled_s_working_fixed.xdmf"), type="xdmf", 
+    periodic=False, tol=tol, side_length=side_length
+)
+print("Mesh imported")
+model.mesh = dolfin.refine(model.mesh)
+print("Mesh refined")
+print("Starting (P) simulation")
+model.sim_p(save=True, meshtype="tkd")
+print("(P) simulation done")
+# print("Starting (T) simulation")
+# x = model.sim_t(hb=False, save=False)
+# solution = model.sim_t(hb=True, save=True, guess=x)
+# print("Done")
