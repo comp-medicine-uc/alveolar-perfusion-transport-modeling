c99486f4d4ecfb0282067b2929fa5a89c913a56a
biherrera <biherrera@uc.cl>
2022-09-22 10:43:37 -0300

    Added flat_patch results data

---
+++ src/mesh_gen/CMakeFiles/mesh_gen_442.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mesh_gen_442.dir/mesh_gen_442.cpp.o"
+  "mesh_gen_442"
+  "mesh_gen_442.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mesh_gen_442.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
