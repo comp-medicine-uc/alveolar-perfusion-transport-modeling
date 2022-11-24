75b78121d11ddca96f6e6601eb220576b1a8c228
Bastián Herrera <biherrera@uc.cl>
2022-11-24 16:27:36 -0300

    Update submit_mumps_default.sh

--- tests/cluster/40_h_repaired_solvers/submit_mumps_default.sh
+++ tests/cluster/40_h_repaired_solvers/submit_mumps_default.sh
@@ -3,7 +3,7 @@
 # Nombre del trabajo
 #SBATCH --job-name=MUMPSdef
 # Archivo de salida
-#SBATCH --output=mumps_default.txt
+#SBATCH --output=mumps_default_1.txt
 # Partición (Cola de y trabajo)
 #SBATCH --partition=512x1024
 # Solicitud de cpus
