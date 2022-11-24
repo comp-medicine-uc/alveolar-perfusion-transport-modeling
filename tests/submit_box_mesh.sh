11b90f2195a5cc3d1cec2df7815f9bd758f3183d
Bastián Herrera <biherrera@uc.cl>
2022-11-24 11:59:22 -0300

    Minor changes

--- tests/submit_box_mesh.sh
+++ tests/submit_box_mesh.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Nombre del trabajo
-#SBATCH --job-name=MUMPSdef
+#SBATCH --job-name=flatp1
 # Archivo de salida
-#SBATCH --output=mumps_default.txt
+#SBATCH --output=flatp1.txt
 # Partición (Cola de y trabajo)
 #SBATCH --partition=512x1024
 # Solicitud de cpus
-#SBATCH --ntasks=40
+#SBATCH --ntasks=20
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-user=biherrera@uc.cl
 #SBATCH --mail-type=ALL
 
-mpirun -n 40 python3 mumps_default.py
+mpirun -n 20 python3 flat_patch_main_1.py
 
