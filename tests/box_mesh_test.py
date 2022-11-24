b3493ebeaaef2c6681afca99e8cc36292a499a17
Bastián Herrera <biherrera@uc.cl>
2022-11-24 12:29:31 -0300

    Update box_mesh_test.py

--- tests/box_mesh_test.py
+++ tests/box_mesh_test.py
@@ -12,11 +12,11 @@ print("Relative path: ", os.getcwd()[:-6])
 
 import dolfin
 print("Dolfin version: ", dolfin.__version__)
-# from fenics import *
+from fenics import *
 from src.model import PerfusionGasExchangeModel
 from src.params import params
     
-box_mesh = dolfin.BoxMesh(Point(0,0,0),Point(5,5,5),10,10,10)
+box_mesh = BoxMesh(Point(0,0,0),Point(5,5,5),10,10,10)
 
 print("Imported src files")
 print("Starting...")
