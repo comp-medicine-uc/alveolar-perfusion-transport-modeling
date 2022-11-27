5fbc63176e2d8d777fce6ec48691b425f558513e
Bastián Herrera <biherrera@uc.cl>
2022-11-27 11:52:52 -0300

    Update submit_big_box_mesh.sh

--- tests/submit_big_box_mesh.sh
+++ tests/submit_big_box_mesh.sh
@@ -7,10 +7,10 @@
 # Partición (Cola de y trabajo)
 #SBATCH --partition=512x1024
 # Solicitud de cpus
-#SBATCH --ntasks=1
+#SBATCH --ntasks=4
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-user=biherrera@uc.cl
 #SBATCH --mail-type=ALL
 
-mpirun -n 1 python3 big_box_mesh.py
+mpirun -n 4 python3 big_box_mesh.py
 
