4b3f307fc55478ae2de4982236fd1646eb1242dd
Bastián Herrera <biherrera@uc.cl>
2023-01-11 16:53:10 -0300

    Added multi test

---
+++ tests/multi.py
@@ -0,0 +1,65 @@
+__author__ = 'biherrera'
+__email__ = 'biherrera@uc.cl'
+
+import sys
+import os
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+sys.path.append(os.getcwd()[:-6])
+print("Relative path: ", os.getcwd()[:-6])
+import dolfin
+from fenics import *
+from src.model import PerfusionGasExchangeModel
+from src.params import params
+
+max_dims = [39.894161224365234, 39.895729064941406, 39.89208984375]
+min_dims = [0.09939099848270416, 0.09558500349521637, 0.1048400029540062]
+
+def run_model(name, fname, solver, preconditioner, boxmesh, n_jobs=15):
+    print(f"Running model on {n_jobs} jobs.")
+    print("Imported src files")
+    print("Starting...")
+    folder = fname + "/" + name
+    path = os.path.join("../../../results-data/", folder)
+    model = PerfusionGasExchangeModel(folder_path=path, params=params, solver='gmres', f_dim = 2, vecf_dim=1)
+    print("Model initialised")
+    
+#     model.import_mesh(
+#         os.path.join("../../../raw-data/40_h_repaired", "40_h_repaired.xdmf"), type="xdmf", 
+#         periodic=False, max_dims=max_dims, min_dims=min_dims, tol=0.1
+#     )
+#     print("Mesh imported")
+    
+    model.mesh = boxmesh
+    model.mesh = dolfin.refine(model.mesh)
+    print("Mesh refined")
+    
+    print("Starting (P) simulation")
+    model.sim_p(save=True, meshtype="tkd")
+    print("(P) simulation done")
+    
+    print("Starting (T) simulation")
+    x = model.sim_t(hb=False, save=True, solver="bicgstab")
+    print("Finished (linear) guess generation")
+    solution = model.sim_t(hb=True, save=True, guess=x, solver=solver, preconditioner=preconditioner)
+    print("Done")
+    
+# amounts = [5,10,20,30,50,100]
+amounts = [5,10,50]
+# edges = [5,25,50]
+edges = [5,50]
+
+for side_length in edges:
+    for amount in amounts:
+        box_mesh = BoxMesh(Point(0,0,0), Point(side_length,side_length,side_length), amount, amount, amount)
+        name = "amount_" + amount
+        fname = "edge_" + side_length
+        try:
+            run_model(name, fname, "bicgstab", "default", box_mesh)
+        except RuntimeError:
+            print(f"La iteración con amount = {amount} y side_length = {side_length}")
+            print("-----------------------------------------------------------------")
+            
+print("Todas las iteraciones terminadas.")
+            
---
+++ tests/submit_multi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Nombre del trabajo
+#SBATCH --job-name=multiBox
+# Archivo de salida
+#SBATCH --output=multiBox.txt
+# Partición (Cola de y trabajo)
+#SBATCH --partition=512x1024
+# Solicitud de cpus
+#SBATCH --ntasks=15
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=biherrera@uc.cl
+#SBATCH --mail-type=ALL
+
+mpirun -n 15 python3 multi.py
+
