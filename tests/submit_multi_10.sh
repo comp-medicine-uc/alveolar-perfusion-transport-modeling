bb85ac840195fd29f546b34296bf0537a24346c0
Bastián Herrera <biherrera@uc.cl>
2023-01-14 04:19:49 -0300

    Moved backup box mesh test files, added n_tasks tests

--- tests/submit_multi_10.sh
+++ tests/submit_multi_10.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Nombre del trabajo
-#SBATCH --job-name=BoxMesh
+#SBATCH --job-name=multiBox
 # Archivo de salida
-#SBATCH --output=box_mesh_test_1_job.txt
+#SBATCH --output=multiBox10.txt
 # Partición (Cola de y trabajo)
 #SBATCH --partition=512x1024
 # Solicitud de cpus
-#SBATCH --ntasks=1
+#SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-user=biherrera@uc.cl
 #SBATCH --mail-type=ALL
 
-mpirun -n 1 python3 box_mesh_test.py
-
+mpirun -n 10 python3 multi.py
---
+++ tests/submit_multi_5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Nombre del trabajo
+#SBATCH --job-name=multiBox
+# Archivo de salida
+#SBATCH --output=multiBox5.txt
+# Partición (Cola de y trabajo)
+#SBATCH --partition=512x1024
+# Solicitud de cpus
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=biherrera@uc.cl
+#SBATCH --mail-type=ALL
+
+mpirun -n 5 python3 multi.py
