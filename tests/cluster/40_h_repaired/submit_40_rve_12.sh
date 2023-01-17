2c6b9b04d9db82877fea9b222627cc8ee8856beb
Bastián Herrera <biherrera@uc.cl>
2023-01-17 17:15:15 -0300

    Update submit_40_rve_12.sh

--- tests/cluster/40_h_repaired/submit_40_rve_12.sh
+++ tests/cluster/40_h_repaired/submit_40_rve_12.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Nombre del trabajo
-#SBATCH --job-name=40rve12_1task
+#SBATCH --job-name=40rve12_3task
 # Archivo de salida
-#SBATCH --output=40_rve_12_1task.txt
+#SBATCH --output=40_rve_12_3task.txt
 # Partición (Cola de y trabajo)
 #SBATCH --partition=512x1024
 # Solicitud de cpus
-#SBATCH --ntasks=1
+#SBATCH --ntasks=3
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-user=biherrera@uc.cl
 #SBATCH --mail-type=ALL
 
-mpirun -n 1 python3 40_rve_12.py
+mpirun -n 3 python3 40_rve_12.py
 
